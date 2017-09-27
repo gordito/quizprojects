f3b9a11a76f04e05b0cb39667106c2ec7bc5672e
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-27 11:38:02 +0200

    Defiended 4 new views (pages) and change name on existing html-files

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Lägger till nya vyer för quiz-sida
+
+from django.shortcuts import render
+
+def startpage (request):
+	return render(request, "startpage.html")
+
+def quiz(request, quiz_number):
+	return render(request, "quiz.html")
+
+def question(request, quiz_number,question_number):
+	return render(request, "question.html")
+
+def completed(request, quiz_number):
+	return render(request, "completed.html")
