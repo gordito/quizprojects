0b927b9b68ab27e006f6b528cf3c04fbae879c0b
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-27 11:57:46 +0200

    Fixed a bug in URL:s with parentheses. Chanage path to bootstrap in every html-page

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -5,9 +5,8 @@ from quiz import views
 
 urlpatterns = [
 	path("",views.startpage),
-	path("quiz/<int:quiz_number>/"),views.quiz,
+	path("quiz/<int:quiz_number>/",views.quiz),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
-	path("quiz/<int:quiz_number>/completed/"), views.completed,
-	
+	path("quiz/<int:quiz_number>/completed/", views.completed),
 ]
 
