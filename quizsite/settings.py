7c1b026911ea867f345c5704a3b4949b5f059a6a
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-27 11:26:25 +0200

    Added statics with css and html-templates in quiz

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#static template
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    )
