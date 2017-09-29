e82d9d3b5e07c36728b9a6b0f9fa10115bfd1a5a
Jantorp <32261186+Jantorp@users.noreply.github.com>
2017-09-29 10:14:50 +0200

    Allowed hosts in settings.py

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')a-ra67!f(4p+one$de9bgl%lyp%zu!^)2@h@a!frvubmgx^rv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
